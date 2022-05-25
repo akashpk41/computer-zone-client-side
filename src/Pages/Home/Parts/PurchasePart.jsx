6152e784fc434bbb11fcb942d0fc5b4c9f5f3f38
akashpk41 <akashpk84100@gmail.com>
2022-05-25 10:29:53 +0600

    axios configuration complete

--- src/Pages/Home/Parts/PurchasePart.jsx
+++ src/Pages/Home/Parts/PurchasePart.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axiosPrivate from '../../../API/axiosPrivate';
 
 const PurchasePart = () => {
      const {params} = useParams()
+     const [test,setTest] = useState([])
      // console.log(params);
+
+     useEffect(() => {
+          axiosPrivate.get(`/parts`)
+          .then(res => console.log(res.data)
+          )
+
+     },[])
+
 
      return (
           <div>
